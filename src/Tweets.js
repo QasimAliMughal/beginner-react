2dd132302fbee05c9a51c22ab0b6b63f3118c72a
unknown <qasimmughal426@gmail.com>
2020-04-26 13:32:25 +0500

    something

---
+++ src/Tweets.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function Tweet() {
+
+    return (
+        <div className="twet">
+            <h3>Tweet Title</h3>
+            <p>Tweet description</p>
+            <h4>Tweet number of likes</h4>
+
+        </div>
+    );
+
+}
+
+export default Tweet;
